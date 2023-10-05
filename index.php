2eb9ae4831f792d9cefc049ebac3fc4b8f18736c
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-05 15:58:31 +0200

    setting up base

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 
-<main>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <article>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php the_content(); ?>
-    </article>
-    <?php endwhile; endif; ?>
-</main>
+<div class="container">
+    <main>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_content(); ?>
+        </article>
+        <?php endwhile; endif; ?>
+    </main>
+</div>
 
 <?php get_footer(); ?>
