2ba12faac56afcc260a1c5ff5f2c4b20a0a1afee
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-09 15:57:12 +0200

    commit

--- assets/lib/functions/hero-image.php
+++ assets/lib/functions/hero-image.php
@@ -1,9 +1,11 @@
 <?php
-
 function heroImage() {
+    $hero_image = get_field('hero-img', 'options');
+    $image_url = esc_url($hero_image['url']);
     ?>
-<div class="hero-image">
-    <div class="hero-text">
+
+<div class="hero_image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(<?php echo $image_url; ?>)">
+    <div class="hero_text">
         <h1 class="site_title solid_underline_wide"><?php echo get_bloginfo('name'); ?></h1>
     </div>
     <div>
@@ -12,5 +14,6 @@ function heroImage() {
         </a>
     </div>
 </div>
+
 <?php }
 ?>
