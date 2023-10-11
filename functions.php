d5bb24b294a4e8b94db42b3049fa86f0c5f35271
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-11 13:34:59 +0200

    commit

--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@
 * require scripts
 */
 
+
+
+
 // Include scripts
 require get_template_directory().'/assets/inc/scripts.php';
 require get_template_directory().'/assets/lib/functions/hero-image.php';
--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -6,6 +6,7 @@
  */
 ?>
 
+
 <article id="post-<?php the_ID(); ?>" <?php post_class('post_item mx-auto'); ?>>
     <?php if ( has_post_thumbnail() ) : ?>
     <div class="post_thumb"><?php the_post_thumbnail(); ?></div>
@@ -14,14 +15,26 @@
             if ( is_single() ) :
                 the_title('<h2 class="post_title">', '</h2>' );
             else:
-                the_title( sprintf( '<h2 class="post_title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+                the_title( sprintf( '<h2 class="post_title"><a class="post_href href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
             endif; 
         ?>
     <h6 class="post_info">
-        <?php echo esc_html_e('by '); ?><a href="<?php echo esc_url(get_author_posts_url( get_the_author_meta('ID')));?>">
+        <?php echo esc_html_e('by '); ?><a class="post_href" href="<?php echo esc_url(get_author_posts_url( get_the_author_meta('ID')));?>">
             <?php esc_html(the_author()); ?></a>
 
         <?php echo esc_html(get_the_date('F ')); echo esc_html(get_the_date('j, ')); echo esc_html(get_the_date('Y')); ?>
     </h6>
-    <?php the_excerpt(); ?>
+
+
+    <?php
+if ( is_single() ) {
+    the_content();
+}
+else {
+    the_excerpt();
+    echo '<a class="link_more_btn" role="button" href="' . get_permalink() . '">Read More</a>';
+}
+?>
+
+
 </article>
