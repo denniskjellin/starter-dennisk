dbaf8bba49b0058a842da7eaf7027e533a17f7c4
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-11 13:42:52 +0200

    commit

--- functions.php
+++ functions.php
@@ -5,8 +5,13 @@
 * require scripts
 */
 
-
-
+/* Excerpt Limit Begin */
+function string_limit_words($string, $word_limit) {
+    $words = explode(' ', $string, ($word_limit + 1));
+    if(count($words) > $word_limit)
+    array_pop($words);
+    return implode(' ', $words);
+}
 
 // Include scripts
 require get_template_directory().'/assets/inc/scripts.php';
--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -6,35 +6,46 @@
  */
 ?>
 
-
 <article id="post-<?php the_ID(); ?>" <?php post_class('post_item mx-auto'); ?>>
-    <?php if ( has_post_thumbnail() ) : ?>
+    <?php if (has_post_thumbnail()) : ?>
     <div class="post_thumb"><?php the_post_thumbnail(); ?></div>
     <?php endif; ?>
-    <?php 
-            if ( is_single() ) :
-                the_title('<h2 class="post_title">', '</h2>' );
-            else:
-                the_title( sprintf( '<h2 class="post_title"><a class="post_href href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-            endif; 
-        ?>
-    <h6 class="post_info">
-        <?php echo esc_html_e('by '); ?><a class="post_href" href="<?php echo esc_url(get_author_posts_url( get_the_author_meta('ID')));?>">
-            <?php esc_html(the_author()); ?></a>
 
-        <?php echo esc_html(get_the_date('F ')); echo esc_html(get_the_date('j, ')); echo esc_html(get_the_date('Y')); ?>
+
+    <?php
+    if (is_single()) :
+        the_title('<h2 class="post_title">', '</h2>');
+    else :
+        the_title(sprintf('<h2 class="post_title"><a class="post_href" href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
+    endif;
+    ?>
+
+
+    <h6 class="post_info">
+        <?php echo esc_html_e('by '); ?><a class="post_href" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
+            <?php the_author(); ?>
+        </a>
+        <?php echo esc_html(get_the_date('F j, Y')); ?>
     </h6>
 
 
     <?php
-if ( is_single() ) {
-    the_content();
-}
-else {
-    the_excerpt();
-    echo '<a class="link_more_btn" role="button" href="' . get_permalink() . '">Read More</a>';
-}
-?>
+    if (is_single()) {
+        the_content(); 
+    } else {
+        $excerpt = get_the_excerpt();
+        $word_count = str_word_count($excerpt);
 
+        if ($word_count > 55) {
+            $limited_excerpt = string_limit_words($excerpt, 55);
+             echo $limited_excerpt;
+             echo '...';
+             echo '<p><a class="link_more_btn" role="button" href="' . get_permalink() . '">Read More</a></p>';
+    
+        } else {
+            the_excerpt();
+        }
+    }
+    ?>
 
 </article>
