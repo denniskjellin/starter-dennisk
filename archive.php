3b1feee5457f8691bda989c623dc5d4c55b696af
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-13 13:19:41 +0200

    commit

--- archive.php
+++ archive.php
@@ -2,7 +2,13 @@
 
 <div id="content_wrapper" class="container pt-5 mt-5 mb-5">
     <div class="col-md-12 text-center">
-        <h2 class="page_title text-uppercase fw-bold">author: <span class="text-lowercase"><?php echo esc_html($author_name); ?></span></h2>
+        <?php if (is_author()) : ?>
+        <h1 class="h3 page_title text-uppercase fw-bold">author: <span class="text-lowercase"><?php echo esc_html($author_name); ?></span></h1>
+        <?php else : ?>
+        <h1 class="h3 page_title text-uppercase fw-bold"><?php the_archive_title(); ?></h1>
+        <?php endif; ?>
+
+
         <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
 
