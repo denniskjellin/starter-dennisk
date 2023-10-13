1fc3c53c5a2552311618b4bd7db734f77e84f16a
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-13 10:00:34 +0200

    commit

--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -7,9 +7,9 @@
 ?>
 
 <!--  -->
-<article id="post-<?php the_ID(); ?>" <?php post_class('post_item mx-auto'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('post_item'); ?>>
     <?php if (!is_single()) : ?>
-    <div class="post_thumb mb-4">
+    <div class="post_thumb">
         <a href="<?php the_permalink(); ?>">
             <?php the_post_thumbnail('large', ['class' => 'rounded', ]); ?>
         </a>
@@ -40,29 +40,33 @@
 
     <!-- if is single, show the content else show the excerpt, if excerpt is long show read more button -->
     <?php if (is_single()) : ?>
-    <?php the_content(); ?>
+    <div class="entry_content">
+        <?php the_content(); ?>
+    </div>
     <?php else : ?>
-    <?php
+
+    <div class="entry_content">
+        <?php
         $excerpt = get_the_excerpt();
         $word_count = str_word_count($excerpt);
 
         if ($word_count > 55) : ?>
-    <?php
+        <?php
             $limited_excerpt = string_limit_words($excerpt, 55);
             echo $limited_excerpt . '...';
             ?>
-    <p><a class="link_more_btn" role="button" href="<?php echo esc_url(get_permalink()); ?>">Read More</a></p>
-    <?php else : ?>
-    <?php the_excerpt(); ?>
-    <?php endif; ?>
-    <?php endif; ?>
-
+        <p><a class="more_btn" role="button" href="<?php echo esc_url(get_permalink()); ?>">Read More</a></p>
+        <?php else : ?>
+        <?php the_excerpt(); ?>
+        <?php endif; ?>
+        <?php endif; ?>
+    </div>
     <!-- If is single, display entry footer with author image  -->
     <?php if (is_single()) : ?>
     <footer class="entry_footer">
         <div class="post_author">
             <div class="col-12">
-                <h4 class="section_title visually-hidden">Post author</h4>
+                <h4 class="visually-hidden">Post author</h4>
             </div>
             <div class="col-12">
                 <a class="post_href" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
@@ -86,9 +90,6 @@
             </span>
         </div>
     </footer>
-
     <?php else : ?>
     <?php endif; ?>
-
-
 </article>
