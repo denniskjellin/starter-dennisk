82d15eb4ec921a7a030707932bb7be30c68539f2
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-13 08:33:19 +0200

    commit

--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -6,6 +6,7 @@
  */
 ?>
 
+<!--  -->
 <article id="post-<?php the_ID(); ?>" <?php post_class('post_item mx-auto'); ?>>
     <?php if (!is_single()) : ?>
     <div class="post_thumb mb-4">
@@ -15,7 +16,7 @@
     </div>
     <?php endif; ?>
 
-
+    <!-- if its a single post, show the h2 title as a navigation link -->
     <?php if (is_single()) : ?>
     <?php else : ?>
     <h2 class="post_title">
@@ -25,6 +26,8 @@
     </h2>
     <?php endif; ?>
 
+
+    <!-- if its not a single page show the post info this way -->
     <?php if (!is_single()) : ?>
     <h6 class="post_info">
         <?php echo esc_html_e('by '); ?>
@@ -35,6 +38,7 @@
     </h6>
     <?php endif; ?>
 
+    <!-- if is single, show the content else show the excerpt, if excerpt is long show read more button -->
     <?php if (is_single()) : ?>
     <?php the_content(); ?>
     <?php else : ?>
@@ -52,4 +56,33 @@
     <?php the_excerpt(); ?>
     <?php endif; ?>
     <?php endif; ?>
+
+    <!-- If is single, display entry footer with author image  -->
+    <?php if (is_single()) : ?>
+    <footer class="entry_footer">
+        <div class="post_author">
+            <h4 class="section_title visually-hidden">
+                Post author</h4>
+            <a class="post_href" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
+                <?php
+            $author_avatar = get_avatar(get_the_author_meta('ID'), 65); 
+            if (strpos($author_avatar, 'default-avatar.png') !== false) {
+                // Author has the default avatar, display your fallback avatar
+                echo '<img src="' . get_template_directory_uri() . '/assets/images/avatar.png" alt="Fallback Avatar" class="avatar">';
+            } else {
+                // Author has a custom avatar, display it
+                echo $author_avatar;
+            }
+            ?>
+            </a>
+            <?php echo esc_html_e('written by '); 
+                the_author_posts_link();
+                ?>
+
+        </div>
+    </footer>
+    <?php else : ?>
+    <?php endif; ?>
+
+
 </article>
