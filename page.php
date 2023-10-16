9db1e60f261455eee399a4be67e12a80e2e68658
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-16 07:52:02 +0200

    commit

--- page.php
+++ page.php
@@ -1,18 +1,35 @@
 <?php get_header(); ?>
+<div class="container-fluid p-0 img_banner_single">
+    <?php
+if (has_post_thumbnail()) {
+    the_post_thumbnail('full');
+} else {
+    ?>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/mountains.jpg" alt="Mountains at dawn">
+    <?php
+}
+?>
+</div>
 
+<div class="container meta_wrapper">
+    <div class="text_banner col-12">
+        <h1 class="entry_title"><?php the_title(); ?></h1>
+        <div class="entry_meta">
+            <!-- secondary title goes here -->
+        </div>
+    </div>
+</div>
+</div>
 
-<div id="content_wrapper" class="container pt-5 mt-5 mb-5">
-    <div class="col-md-12 text-center">
-        <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-
+<div id="content_wrapper" class="container mt-4 mb-4">
+    <div class="col-md-12">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <?php get_template_part('template-parts/content/content', 'page'); ?>
-
-        <?php endwhile; ?>
-        <?php else : ?>
+        <?php endwhile; else : ?>
         <?php get_template_part('template-parts/content/content', 'none'); ?>
         <?php endif; ?>
     </div>
+
 
 </div>
 
--- template-parts/content/content-page.php
+++
@@ -1,2 +0,0 @@
-<h1>Page</h1>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, dolore.
